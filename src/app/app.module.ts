35f8e5c7d08be52f9439012cdee17b0e26a520fd
tanyushanel <tanyushanel@users.noreply.github.com>
2020-12-22 18:25:51 +0300

    main components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SliderComponent } from './slider/slider.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    AboutComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
