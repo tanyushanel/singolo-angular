e98c59242e331ab70930f4163025798cafb6d689
tanyushanel <tanyushanel@users.noreply.github.com>
2021-01-21 18:20:37 +0300

    fix ui

--- src/app/slider/phones/phone/phone.component.ts
+++ src/app/slider/phones/phone/phone.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-phone',
   templateUrl: './phone.component.html',
-  styleUrls: ['./phone.component.scss']
+  styleUrls: ['./phone.component.scss'],
 })
 export class PhoneComponent implements OnInit {
+  @Input() isHorizontal: boolean = false;
+  imgHorizont: string = '../../../../assets/phone-hor-display.png';
+  imgVertical: string = '../../../../assets/phone-vertical-display.png';
 
-  constructor() { }
+  shadowHorizont: string =
+    '25px -25px #ce5d56, 20px -20px #ce5d56, 15px -15px #ce5d56, 10px -10px #ce5d56, 5px -5px #ce5d56';
+  shadowVertical: string =
+    '25px 25px #ce5d56, 20px 20px #ce5d56, 15px 15px #ce5d56, 10px 10px #ce5d56, 5px 5px #ce5d56';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
